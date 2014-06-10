653b0e793748326c0c9e68d6c56b3ad58d995689
Seven90 <stevesurya@gmail.com>
2014-06-10 10:58:40 -0400

    Added rolodex.rb

--- crm.rb
+++ crm.rb
@@ -1,4 +1,5 @@
 require_relative 'contact'
+require_relative 'rolodex'
 require 'sinatra'
 
 get '/' do 
@@ -6,4 +7,11 @@ get '/' do
 	erb :index
 end
 
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 
+  erb :contacts
+end
