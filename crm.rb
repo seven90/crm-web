10f1e57ed265ccb0eef89d9911e08996189c4b27
Seven90 <stevesurya@gmail.com>
2014-06-12 11:26:22 -0400

    Added database

--- crm.rb
+++ crm.rb
@@ -1,10 +1,26 @@
-require_relative 'contact'
 require_relative 'rolodex'
+
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
 @@rolodex = Rolodex.new
 
-@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+# @@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+class Contact
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :note, String
+
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do 
   @crm_app_name = "My CRM"
